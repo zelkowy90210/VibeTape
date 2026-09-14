c4e618da1eeae4fecc826efca1d4f6a8bb5ee3db
elozelo <jlew1305@gmail.com>
2026-09-14 18:05:15 +0200

    Dodano komunikację z jamendo api przez backend

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,29 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send(`Backend dziala na porcie: ${PORT}`)
 })
+app.post('/api/get-song', async (req,res) => {
+    try{
+        const gatunek = req.body.gatunek
+        const clientId = process.env.JAMENDO_CLIENT_ID;
+        const jamendoUrl = `https://api.jamendo.com/v3.0/tracks/?client_id=${clientId}&format=json&tags=${gatunek}`
+        console.log("Działa")
+        const reponse = await fetch(jamendoUrl)
+        const data = await reponse.json();
+        const track = data.results[0]
+        res.json({
+            song:{
+                title: track.name,
+                artist: track.artist_name
+            }
+        })
+        console.log(`Znalazłem utwór: ${track.name}`)
+
+    }
+    catch(err){
+        console.log(`Error o nazwie: ${err}`)
+        res.status(500).json({error: "Błąd serwera"})
+    }
+})
 app.listen(PORT, () =>{
     console.log(`Serwer uruchomiony, sprawdz localhost:${PORT}`)
 })
